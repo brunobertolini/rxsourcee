b9634be712d29596dbc4e9f92c5b731be7ef98f3
Bruno Bertolini <dev@brunobertolini.com>
2019-01-24 10:53:08 -0200

    fix: recreate resource

--- src/useResource.js
+++ src/useResource.js
@@ -2,7 +2,7 @@ import { useMemo, useEffect } from 'react'
 import { useStateRx } from './useStateRx'
 
 export const useResource = (resource, options) => {
-  const res = useMemo(() => resource(options), [resource, options])
+  const res = useMemo(() => resource(options), [])
 
   const [state] = useStateRx(res.getState())
   const [ctx] = useStateRx(res.getContext())
